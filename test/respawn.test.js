05da65625b99327122a6531debd47ab402fef7a6
Atsuya Takagi <asoftonight@gmail.com>
2012-09-09 04:08:20 -0700

    add respawn

--- test/respawn.test.js
+++ test/respawn.test.js
@@ -1,3 +1,5 @@
+var cp = require('child_process');
+
 var helper = require('./support/helper')
   , Qilin = require(global.test.root + '/lib/qilin');
 
@@ -5,16 +7,54 @@ describe('Qilin', function() {
   var qilin;
 
   beforeEach(function(done) {
-    //qilin = new Qilin(
-    //    {
-    //    }
-    //  , {
-    //    }
-    //);
+    qilin = new Qilin(
+        {
+            exec: global.test.testRoot + '/resources/worker.js'
+          , args: []
+          , silent: false
+        }
+      , {
+            workers: 2
+        }
+    );
+    qilin.start(function(error) {
+      if (error) {
+        throw error;
+      }
+      done();
+    });
   });
 
   afterEach(function(done) {
+    qilin.shutdown(function(error) {
+      if (error) {
+        throw error;
+      }
+      done();
+    });
   });
 
-  it('respawns killed process');
+  it('respawns killed process', function(done) {
+    qilin.on('worker.respawn', function() {
+      done();
+    });
+
+    cp.exec('ps axu | grep worker.js', function(error, stdout, stderr) {
+      if (error) {
+        throw error;
+      }
+
+      var lines = stdout.split('\n');
+      lines = lines.filter(function(line) {
+        return line.indexOf('node') !== -1;
+      });
+      var values = lines[0].split(' ');
+      values = values.filter(function(value) {
+        return value.trim() !== '';
+      });
+      var pid = parseInt(values[1], 10);
+
+      process.kill(pid);
+    });
+  });
 });
