d113209ece1b2c025012862f4894f7a2a7b39e7f
Atsuya Takagi <asoftonight@gmail.com>
2012-09-08 23:10:48 -0700

    ok, i think i know what to do now

--- scratch/app.js
+++ scratch/app.js
@@ -1,9 +1,21 @@
-var http = require('http');
+var http = require('http')
+  , fs = require('fs')
+  , util = require('util');
+
+function log(message) {
+  fs.appendFile('./log.txt', message);
+}
 
 var httpServer = http.createServer(function(request, response) {
-  console.log('served by: %d', process.pid);
-  response.writeHead(200);
-  response.end("hello world\n");
+  var message = util.format('served by: %d\n', process.pid);
+  log(message);
+
+  setTimeout(function() {
+    response.writeHead(200);
+    response.end("hello world by aaaaaaaaaaa" + process.pid + "\n");
+  }, 60000);
 });
+httpServer.on('close', function() { log(util.format('closing http server: %d\n', process.pid)); });
+httpServer.on('clientError', function() { log(util.format('client error: %d\n', process.pid)); });
 httpServer.listen(3000, 'localhost');
-console.log('http server is running on localhost:3000');
+log('http server is running on localhost:3000\n');
--- scratch/muska.js
+++ scratch/muska.js
@@ -1,30 +1,55 @@
 var cluster = require('cluster')
-  , childProcess = require('child_process');
+  , childProcess = require('child_process')
+  , fs = require('fs')
+  , util = require('util');
+
+function log(message) {
+  fs.appendFile('master.txt', message);
+}
+
+function fork() {
+  for(var index = 0; index < 3; index++) {
+    cluster.fork();
+  }
+}
+
+function workers() {
+  log('workers:\n');
+  for (var id in cluster.workers) {
+    var worker = cluster.workers[id];
+    log(util.format('\t%d: %d\n', worker.id, worker.process.pid));
+  }
+}
 
 cluster.setupMaster({
     exec : './app.js'
   , args : []
   , silent : false
 });
-for(var index = 0; index < 3; index++) {
-  cluster.fork();
-}
+
+fork();
 
 cluster.on('exit', function(worker, code, signal) {
-  console.log('worker ' + worker.process.pid + ' died');
+  log('worker ' + worker.process.pid + ' died\n');
   if (!worker.suicide) {
-    console.log('since it was not a suicide, let it reborn!');
+    log('since it was not a suicide, let it reborn!\n');
     cluster.fork();
   }
 });
 
 process.on('SIGUSR2', function() {
-  console.log('got SIGUSR2');
+  log('got SIGUSR2. killing those.\n');
+  workers();
   cluster.disconnect(function() {
-    console.log('all killed');
+    log('all killed\n');
   });
   //spawn('node', ['muska.js'], { cwd: __dirname });
-  childProcess.execFile('node', ['muska.js'], { cwd: __dirname });
+  //childProcess.execFile('node', ['muska.js'], { cwd: __dirname });
+
+  fork();
+  log('forked\n');
 });
 
-console.log(__dirname);
+process.on('SIGUSR1', function() {
+  workers();
+});
