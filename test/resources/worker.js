36e32d9aa794ceec30c441eb9a59c2a19341d41e
Atsuya Takagi <asoftonight@gmail.com>
2012-09-10 23:08:21 -0700

    put back domain

--- test/resources/worker.js
+++ test/resources/worker.js
@@ -10,7 +10,7 @@ var logging = require('../../lib/logging');
 
 var httpServer = http.createServer(function(request, response) {
   response.writeHead(200);
-  response.end('qilin test server2');
+  response.end('qilin test server');
 });
 httpServer.listen(3001, 'localhost');
 logging.info(util.format('worker started: %d', process.pid));
